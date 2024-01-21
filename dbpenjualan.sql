4203121edda25371a00cca7a28cacd5d468fc9eb
Alvinbs <alvinbagassetiono7@gmail.com>
2024-01-21 14:50:27 +0700

    first commit

---
+++ dbpenjualan.sql
@@ -0,0 +1,93 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 21 Jan 2024 pada 08.35
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbpenjualan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `akun`
+--
+
+CREATE TABLE `akun` (
+  `id_akun` int(20) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `akun`
+--
+
+INSERT INTO `akun` (`id_akun`, `username`, `password`) VALUES
+(1, 'Koh Gilang', '12345');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `datatransaksi`
+--
+
+CREATE TABLE `datatransaksi` (
+  `id_transaksi` int(20) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `hp` varchar(100) NOT NULL,
+  `namabarang` varchar(100) NOT NULL,
+  `harga` int(100) NOT NULL,
+  `alamat` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `akun`
+--
+ALTER TABLE `akun`
+  ADD PRIMARY KEY (`id_akun`);
+
+--
+-- Indeks untuk tabel `datatransaksi`
+--
+ALTER TABLE `datatransaksi`
+  ADD PRIMARY KEY (`id_transaksi`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `akun`
+--
+ALTER TABLE `akun`
+  MODIFY `id_akun` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `datatransaksi`
+--
+ALTER TABLE `datatransaksi`
+  MODIFY `id_transaksi` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ home.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Waroenk Ramen & Sushi</title>
+
+    <!-- cdn icon link  -->
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+    <!-- custom css file  -->
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+
+  <body>
+    <!-- header section start here  -->
+    <header class="header">
+      <div class="logoContent">
+        <a href="#" class="logo"><img src="Ramen 1.png" alt="" /></a>
+        <h1 class="logoName">Koh Gilang</h1>
+      </div>
+
+      <nav class="navbar">
+        <a href="#home">Home</a>
+        <a href="#product">Product</a>
+        <a href="#blogs">About</a>
+        <a href="#contact">Contact</a>
+        <a href="index.php">Log Out</a>
+      </nav>
+
+      <div class="icon">
+        <i class="fas fa-search" id="search"></i>
+        <i class="fas fa-bars" id="menu-bar"></i>
+      </div>
+
+      <div class="search">
+        <input type="search" placeholder="search..." />
+      </div>
+    </header>
+    <!-- header section end here  -->
+
+    <!-- home section start here  -->
+    <section class="home" id="home">
+      <div class="homeContent">
+        <h2>Selamat Datang Di Waroenk Ramen & Sushi Koh Gilang</h2>
+        <p>
+          
+Waroenk Sushi & Ramen Koh Gilang
+Di Kota Tegal, setiap hidangan adalah narasi rasa, sebuah perjalanan dimana laut bertemu daratan. Dengan harga terjangkau, kami mengundang Anda ke dunia dimana tradisi Jepang menyatu dalam harmoni sempurna.
+        </p>
+        </div>
+						<div class="home__slider">
+							<div class="home__image">
+								<img src="Ramen 1.jpeg" alt="home img" class="home__img img">
+							</div>
+        <div class="home-btn">
+          <a href="#product"><button>Waroenk Koh Gilang</button></a>
+        </div>
+      </div>
+    </section>
+
+    <!-- home section end here  -->
+
+    <!-- product section start here  -->
+    <section class="product" id="product">
+      <div class="heading">
+        <h2>Varian Menu Waroenk Koh Gilang</h2>
+      </div>
+      <div class="grid-container">
+        <div class="grid-item">
+          <img src="Ramen 2.jpg" alt="Image 1">
+          <h3>Shoyu Ramen</h3>
+          <p>Rp 50.000</p>
+          <button class="buy-now-button"><a href="form_pembelian.php">Beli Sekarang</a></button>
+        </div>
+        <div class="grid-item">
+          <img src="Ramen 1.jpeg" alt="Image 2">
+          <h3>Shio Ramen</h3>
+          <p>Rp 40.000</p>
+          <button class="buy-now-button"><a href="form_pembelian.php">Beli Sekarang</a></button>
+        </div>
+        <div class="grid-item">
+          <img src="Sushi 1.jpeg" alt="Image 3">
+          <h3>Maki Sushi </h3>
+          <p>Rp 45.000</p>
+          <button class="buy-now-button"><a href="form_pembelian.php">Beli Sekarang</a></button>
+        </div>
+        <div class="grid-item">
+          <img src="Sushi 2.jpg" alt="Image 3">
+          <h3>Nigiri Sushi</h3>
+          <p>Rp 20.000</p>
+          <button class="buy-now-button"><a href="form_pembelian.php">Beli Sekarang</a></button>
+        </div>
+        <div class="grid-item">
+          <img src="Sushi 3.jpeg" alt="Image 3">
+          <h3>Inarizushi</h3>
+          <p>Rp 60.000</p>
+          <button class="buy-now-button"><a href="form_pembelian.php">Beli Sekarang</a></button>
+        </div>
+        <div class="grid-item">
+          <img src="Uramaki.jpeg" alt="Image 3">
+          <h3>Uramaki</h3>
+          <p>Rp 48.000</p>
+          <button class="buy-now-button"><a href="form_pembelian.php">Beli Sekarang</a></button>
+        </div>
+        <div class="grid-item">
+          <img src="Chirashi.jpg" alt="Image 3">
+          <h3>Chirashi</h3>
+          <p>Rp 10.000</p>
+          <button class="buy-now-button"><a href="form_pembelian.php">Beli Sekarang</a></button>
+        </div>
+      </div>
+      
+    <!-- product section end here  -->
+
+    <!-- blogs section start here  -->
+    <section class="blogs" id="blogs">
+      <div class="swiper blogs-row">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide box">
+            <div class="img">
+              <img src="" alt="" />
+            </div>
+            <div class="content">
+              <h3>Tentang  Waroenk Koh Gilang</h3>
+              <h4>Waroenk Koh Gilang Yang Berletak Di Kramat Kabupaten Jawa Tengah Kota Tegal, Yang Bediri Taun 2023 Yang Membuka Usaha Kuliner Yaitu Ramen & Sushi Dengan Harga Yang Terjangkau Untuk Pelajar  </h4>
+            </div>
+
+    <!-- blogs section ends here  -->
+    
+    <!-- footer section start here  -->
+
+    <footer class="footer" id="contact">
+      <div class="box-container">
+        <div class="mainBox">
+          <div class="content">
+            <h1 class="logoName">Waroenk Koh Gilang</h1>
+          </div>
+
+          <p>
+            Terima kasih telah membeli di Waroenk Koh Gilang semoga anda suka dan ingin membeli lagi
+          </p>
+        </div>
+        <div class="box">
+          <h3>Quick link</h3>
+          <a href="#home"> <i class="fas fa-arrow-right"></i>Home</a>
+          <a href="#product"> <i class="fas fa-arrow-right"></i>product</a>
+          <a href="#blogs"> <i class="fas fa-arrow-right"></i>about</a>
+          <a href="#contact"> <i class="fas fa-arrow-right"></i>contact</a>
+        </div>
+        <div class="box">
+          <h3>Kontak kami</h3>
+          <a href="https://api.whatsapp.com/send?phone=6281998958435&text=Saya%20Ingin%20Memesan%20Ramen%20&%20Sushinya%20Kak"><i class="fas fa-phone"></i>+6281998958435</a>
+          <a href="#">
+            <i class="fas fa-envelope"></i>gilangdarab@gmail.com</a
+          >
+        </div>
+      </div>
+      <div class="share">
+        <a href="https://www.instagram.com/gll.stoneheart?igsh=NnlrZ2x5c2g0cnQ3" class="fab fa-istagram"></a>
+        <a href="#" class="fab fa-instagram"></a>
+      </div>
+      <div class="credit">
+        created by <span>Gilang Ramadhan</span> | Est 2023 !
+      </div>
+    </footer>
+
+    <!-- swiper js link  -->
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+
+    <!-- custom js file  -->
+    <script src="js/main.js"></script>
+  </body>
+</html>
